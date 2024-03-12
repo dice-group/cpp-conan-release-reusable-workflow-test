bbc6d5ab8810f56eaa0121d3cc8f9e66adbb9362
Liss Heidrich <31625940+Clueliss@users.noreply.github.com>
2024-03-12 09:51:09 +0100

    revert all changes

--- include/cpp-conan-release-reusable-workflow-test.hpp
+++ include/cpp-conan-release-reusable-workflow-test.hpp
@@ -7,6 +7,6 @@ static inline std::string hello() {
     return "Hello world!";
 }
 
-inline constexpr char abi_version[] = "1";
+inline constexpr char abi_version[] = "0";
 
 #endif //CPP_CONAN_RELEASE_REUSABLE_WORKFLOW_TEST_CPP_CONAN_RELEASE_REUSABLE_WORKFLOW_TEST_HPP

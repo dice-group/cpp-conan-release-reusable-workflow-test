b4b1ba2511748afe6847ecec26e01e7b902a8615
Adrian @ Tentris <adrian@tentris.io>
2026-07-07 12:04:50 +0200

    Use tagged version. (#12)
    * Use tagged version.
    
    * Try out v1.0.2
    
    * Try out v1.0.2
    
    * Try out v1.0.2
    
    * Try out v1.0.2
    
    * Try out v1.0.2
    
    * Try out v1.0.2
    
    * Try out v1.0.2
    
    * Try out v1.0.2
    
    * Try out v1.0.2
    
    * Try out v1.0.2
    
    * Try out v1.0.2
    
    * Try out v1.0.2
    
    * Try out v1.0.2

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,13 +1,17 @@
 import os
-from conans import ConanFile
-from conan.tools.cmake import CMake, CMakeToolchain
-from conan.tools.layout import cmake_layout
 
-required_conan_version = ">=1.43.0"
+from conan import ConanFile
+from conan.tools.build import can_run
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+
+required_conan_version = ">=2.0.0"
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -22,4 +26,6 @@ class TestPackageConan(ConanFile):
         cmake.build()
 
     def test(self):
-        self.run(os.path.join(self.cpp.build.bindirs[0], "example"), run_environment=True)
+        if can_run(self):
+            cmd = os.path.join(self.cpp.build.bindir, "example")
+            self.run(cmd, env="conanrun")

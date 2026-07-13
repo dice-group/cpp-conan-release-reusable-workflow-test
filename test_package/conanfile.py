6ab9f816845dc5d794b0589010b39848242eecd3
Adrian Macal <adrian@tentris.io>
2026-07-13 15:09:45 +0200

    Fix the build.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -6,10 +6,9 @@ from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
 
 required_conan_version = ">=2.0.0"
 
-
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps", "CMakeToolchain"
+    generators = "CMakeDeps"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
